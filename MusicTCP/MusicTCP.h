e5490564592e1e51acff7238040170180f433646
CapsuleMaglev <gcattani1@gmail.com>
2016-10-25 19:09:03 -0200

    'initial'

---
+++ MusicTCP/MusicTCP.h
@@ -0,0 +1,32 @@
+
+// MusicTCP.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMusicTCPApp:
+// See MusicTCP.cpp for the implementation of this class
+//
+
+class CMusicTCPApp : public CWinApp
+{
+public:
+	CMusicTCPApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMusicTCPApp theApp;
